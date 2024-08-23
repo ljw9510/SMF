7797a1021ace2039e80a9307872c3e063a5f3652
ywei224 <ywei224@ap2002.chtc.wisc.edu>
2024-08-23 15:08:58 -0500

    Modified notebooks/MNIST_test.py

--- notebooks/MNIST_test.py
+++ notebooks/MNIST_test.py
@@ -17,58 +17,18 @@ import sys
 sys.path.append('../src')  # Go one level up and then into src
 from SMF import SMF_BCD
 from SMF_torch import smf
-# Load data from https://www.openml.org/d/554
-X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
-# X = X.values  ### Uncomment this line if you are having type errors in plotting. It is loading as a pandas dataframe, but our indexing is for numpy array. 
-X = X / 255.
-
-print('X.shape', X.shape)
-print('y.shape', y.shape)
-
-'''
-Each row of X is a vectroization of an image of 28 x 28 = 784 pixels.  
-The corresponding row of y holds the true class label from {0,1, .. , 9}.
-'''
-
-### Change the dimension of y for SMF_BCD
-y_matrix = np.zeros((9, y.shape[0]))
-for i in range(y.shape[0]):
-    label = int(y[i])
-    if label == 0:
-        continue
-    else:
-        y_matrix[label-1, i] = 1
-
-print(y_matrix.shape)
-
-### Scale the input data
-from sklearn import preprocessing
-
-scaler = preprocessing.StandardScaler().fit(X)
-
-X= scaler.transform(X)
-
-print(X.shape)
-
-"""
-#data = pd.read_csv('../Data/Pancreatic_GSE16515.csv')
-data = pd.read_csv('../Data/Breast_GSE70947.csv')
-
-print(data.shape)
-y = data['type'].values
-y = [1 if y[i] == 'breast_adenocarcinoma' else 0 for i in range(len(y))]
-y = np.array(y)
-X = data.drop(columns=['samples', 'type']).values
-X = (X - np.mean(X, axis = 0))/(100*np.std(X, axis = 0)) # normalization
-"""
 
 
-hidden_size = 4
+X = np.loadtxt("./MNIST_X.txt")
+y = np.loadtxt("./MNIST_y.txt")
+
+
+hidden_size = 2
 
 Accuracy = []
 test_size = 0.5
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, y_matrix.T, test_size=test_size, random_state=2)
+X_train, X_test, Y_train, Y_test = train_test_split(X.T, y.T, test_size=test_size, random_state=2)
 # print(X_train.shape)
 # print(Y_train.shape)
 
@@ -80,7 +40,7 @@ y_test = Variable(torch.from_numpy(Y_test)).long()
 # print(f"y_test: {y_test.shape}")
 
 smf_model = smf(X_train, y_train, hidden_size=hidden_size, device='cuda')
-smf_model.fit(num_epochs=500,
+smf_model.fit(num_epochs=100,
                lr_classification=0.01,
                lr_matrix_factorization=0.01,
                xi=1,
@@ -89,6 +49,6 @@ smf_model.fit(num_epochs=500,
                H_nonnegativity=True,
                test_data=[X_test, y_test],
                record_recons_error=True)
-Accuracy.append(smf_model.test(X_test, y_test))
+# Accuracy.append(smf_model.test(X_test, y_test))
 
 
